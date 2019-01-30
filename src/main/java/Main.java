74a1308bed95e2145bbbced331c7979c21e391e0
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-01-30 20:08:13 +0200

    project

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import retail.Customer;
+
+import javax.persistence.metamodel.EntityType;
+
+class Main {
+    private static final SessionFactory SESSION_FACTORY;
+
+    static {
+        // A SessionFactory is set up once for an application!
+        final var REGISTRY = new StandardServiceRegistryBuilder()
+                .configure()
+                // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            SESSION_FACTORY = new MetadataSources(REGISTRY)
+                    .buildMetadata().buildSessionFactory();
+        } catch (HibernateException ex) {
+            StandardServiceRegistryBuilder.destroy(REGISTRY);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static org.hibernate.Session getSession() throws HibernateException {
+        return SESSION_FACTORY.openSession();
+    }
+
+    public static void main(final String[] args) {
+        try (var session = getSession()) {
+            System.out.println("querying all the managed entities...");
+            session
+                    .getSessionFactory()
+                    .getMetamodel()
+                    .getEntities()
+                    .stream()
+                    .map(EntityType::getName)
+                    .map(entityName -> session.createQuery("from " + entityName))
+                    .forEach(query -> {
+                        System.out.println("executing: " + query.getQueryString());
+                        for (var o : query.list())
+                            System.out.println("  " + o);
+                    });
+            var tx = session.beginTransaction();
+            session.save(new Customer(
+                    "+380-98-456-43-54",
+                    "Dima",
+                    "Pochta",
+                    "Vasila St.",
+                    "Warsaw"
+            ));
+            tx.commit();
+            session.close();
+        }
+    }
+}
---
+++ src/main/java/retail/Order.java
@@ -0,0 +1,95 @@
+package retail;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Basic;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "orders", schema = "public", catalog = "Retail_Service")
+public class Order {
+    private int orderNumber;
+    private String toStreet;
+    private String toCity;
+    private Timestamp shipDate;
+    private Customer customersByPhone;
+
+    @Id
+    @Column(name = "order_number", nullable = false)
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    @Basic
+    @Column(name = "toStreet", nullable = false, length = -1)
+    public String getToStreet() {
+        return toStreet;
+    }
+
+    public void setToStreet(String toStreet) {
+        this.toStreet = toStreet;
+    }
+
+    @Basic
+    @Column(name = "toCity", nullable = false, length = -1)
+    public String getToCity() {
+        return toCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    @Basic
+    @Column(name = "shipDate", nullable = false)
+    public Timestamp getShipDate() {
+        return shipDate;
+    }
+
+    public void setShipDate(Timestamp shipDate) {
+        this.shipDate = shipDate;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var order = (Order) o;
+        return orderNumber == order.orderNumber &&
+                Objects.equals(toStreet, order.toStreet) &&
+                Objects.equals(toCity, order.toCity) &&
+                Objects.equals(shipDate, order.shipDate);
+    }
+
+    public int hashCode() {
+        return Objects.hash(orderNumber, toStreet, toCity, shipDate);
+    }
+
+    public String toString() {
+        return "Order{" +
+                "orderNumber=" + orderNumber +
+                ", toStreet='" + toStreet + '\'' +
+                ", toCity='" + toCity + '\'' +
+                ", shipDate=" + shipDate +
+                ", customersByPhone=" + customersByPhone +
+                '}';
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "phone", referencedColumnName = "phone", nullable = false)
+    public Customer getCustomersByPhone() {
+        return customersByPhone;
+    }
+
+    public void setCustomersByPhone(Customer customersByPhone) {
+        this.customersByPhone = customersByPhone;
+    }
+}
